b97f09536160494f22f597a1fe7783858255e3b2
Gothikxx <cacakuli30@gmail.com>
2026-03-03 22:58:48 +0100

    Add notification script for Zengor
    This script sends a notification in Roblox and cleans up existing notifications.

---
+++ Zengor.lua
@@ -0,0 +1,25 @@
+local coreGui = game:GetService("CoreGui")
+local robloxGui = coreGui:FindFirstChild("RobloxGui")
+if robloxGui then
+    local endTime = tick() + 3
+    while tick() < endTime do
+        local notificationFrame = robloxGui:FindFirstChild("NotificationFrame")
+        if notificationFrame then
+            for _, child in ipairs(notificationFrame:GetChildren()) do
+                if child:IsA("Frame") then
+                    child:Destroy()
+                end
+            end
+        end
+        wait(0.05)
+    end
+end
+
+local miAssetID = 100390260385968 
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Zengor",
+    Text = "Injected! Thanks for choosing Zengor.",
+    Icon = "rbxassetid://" .. miAssetID, -- Se forma la ruta completa aquí
+    Duration = 5
+})
